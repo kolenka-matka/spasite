3216e2822c09700cd565ae427dd7ad418649eed7
Spalesta <spalesta@gmail.com>
2022-01-30 02:26:21 +0300

    немного изменила процесс авторизации (чуть-чуть совсем)

--- girorp/urls.py
+++ girorp/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
     path('', views.index, name='index'), # путь к главной странице сайта
     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     #path(r'^login/$', views.user_login, name='login'),
-    path(r'^register/$', views.register, name='register')
+    path(r'^register/$', views.register, name='register'),
     path('login/', 'django.contrib.auth.views.login', name='login'),
     path('logout/', 'django.contrib.auth.views.logout_view', name='logout'),
     path('logout-then-login/', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
     path(r'^$', views.dashboard, name='dashboard'),
     path('password-change/', 'django.contrib.auth.views.password_change', name='password_change'),
-    path('password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done')
-    path('movies_choice/', views.choose_movies(), name='movies')
+    path('password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
+    path('movies_choice/', views.choose_movies, name='movies'),
     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ]
--- girorp/views.py
+++ girorp/views.py
@@ -26,12 +26,11 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    response = HttpResponse()
-                    return HttpResponse('Authenticated successfully')
+                    return render(request, 'account/successful_auth.html')
                 else:
                     return HttpResponse('Disabled account')
             else:
-                return HttpResponse('Invalid login')
+                return render(request, 'account/invalid_login.html')
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
